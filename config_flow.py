3ce07b2dbc9b700100399d987873673e6826fa0d
pawkakol1 <pawel.kakol130@gmail.com>
2024-01-19 22:04:09 +0100

    Deprecated methods from Modbus plaftform, and temporary logic deleted

--- config_flow.py
+++ config_flow.py
@@ -7,61 +7,35 @@ import logging
 
 import voluptuous as vol
 
-from homeassistant import (
-    config_entries,
-)
 from homeassistant.config_entries import (
     ConfigFlow,
     ConfigEntry,
 )
 from homeassistant.core import callback
-from homeassistant.data_entry_flow import FlowResult
-from homeassistant.helpers.schema_config_entry_flow import (
-    SchemaFlowFormStep,
-    SchemaOptionsFlowHandler,
-)
 import homeassistant.helpers.config_validation as cv
 
-from homeassistant.components.modbus import (
-    DEFAULT_HUB,
-)
 from homeassistant.components.modbus.const import (
     DataType,
     CALL_TYPE_REGISTER_HOLDING,
     CONF_DATA_TYPE,
     CONF_INPUT_TYPE,
-    CONF_LAZY_ERROR,
-    DEFAULT_HUB,
-    DEFAULT_SCAN_INTERVAL,
-    DEFAULT_SLAVE,
-    MODBUS_DOMAIN,
+    DEFAULT_SLAVE
 )
 
-from homeassistant.components.cover import (
-    DEVICE_CLASS_BLIND
-)
+from homeassistant.components.cover import CoverDeviceClass
 
 from homeassistant.const import (
     CONF_ADDRESS,
-    CONF_DEVICE,
-    CONF_DEVICES,
-    CONF_DEVICE_CLASS,
-    CONF_FRIENDLY_NAME,
-    CONF_HOST,
-    CONF_METHOD,
     CONF_NAME,
-    CONF_ID,
     CONF_SCAN_INTERVAL,
-    CONF_SLAVE,
-    TEMP_CELSIUS,
+    CONF_SLAVE
 )
 from .const import (
     CONF_HUB_NAME,
     CONF_INIT,
     CONF_USER,
     DEFAULT_NAME,
-    DOMAIN,
-    PLATFORMS
+    DOMAIN
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -104,7 +78,7 @@ class ModbusBlinderComponentConfigFlow(ConfigFlow, domain=DOMAIN):
             scan_interval = user_input.get(CONF_SCAN_INTERVAL)
             cur_pos_addr = user_input.get(CONF_ADDRESS)
             input_type = CALL_TYPE_REGISTER_HOLDING
-            #dev_class = DEVICE_CLASS_BLIND
+            #dev_class = CoverDeviceClass.BLIND
             
 
             if not errors:
@@ -121,7 +95,6 @@ class ModbusBlinderComponentConfigFlow(ConfigFlow, domain=DOMAIN):
                         CONF_DATA_TYPE: DataType.UINT16.value,
                         CONF_ADDRESS: cur_pos_addr,
                         CONF_INPUT_TYPE: input_type,
-                        CONF_LAZY_ERROR: 0,
                         #CONF_DEVICE_CLASS: dev_class,
                     },
                 )
@@ -161,7 +134,7 @@ class ModbusBlinderComponentOptionsFlowHandler(ConfigFlow):
             scan_interval = user_input.get(CONF_SCAN_INTERVAL)
             cur_pos_addr = user_input.get(CONF_ADDRESS)
             input_type = CALL_TYPE_REGISTER_HOLDING
-            #dev_class = DEVICE_CLASS_BLIND
+            #dev_class = CoverDeviceClass.BLIND
             _LOGGER.debug(f"Editing2")
             
 
@@ -180,7 +153,6 @@ class ModbusBlinderComponentOptionsFlowHandler(ConfigFlow):
                         CONF_DATA_TYPE: DataType.UINT16.value,
                         CONF_ADDRESS: cur_pos_addr,
                         CONF_INPUT_TYPE: input_type,
-                        CONF_LAZY_ERROR: 0,
                         #CONF_DEVICE_CLASS: dev_class,
                     }
                 
@@ -208,66 +180,6 @@ class ModbusBlinderComponentOptionsFlowHandler(ConfigFlow):
                     vol.Required(CONF_SLAVE, default=defaults[CONF_SLAVE]): cv.positive_int,
                     vol.Required(CONF_SCAN_INTERVAL, default=defaults[CONF_SCAN_INTERVAL]): cv.positive_int,
                     vol.Required(CONF_ADDRESS, default=defaults[CONF_ADDRESS]): cv.positive_int,
-                }
-            ),
-            errors=errors,
-        )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-class DnsIPOptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle a option config flow for dnsip integration."""
-
-    def __init__(self, entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.entry = entry
-
-    async def async_step_init(
-        self, user_input: dict[str, Any] | None = None
-    ) -> FlowResult:
-        """Manage the options."""
-        errors = {}
-        if user_input is not None:
-            validate = await async_validate_hostname(
-                self.entry.data[CONF_HOSTNAME],
-                user_input[CONF_RESOLVER],
-                user_input[CONF_RESOLVER_IPV6],
-            )
-
-            if validate[CONF_IPV4] is False and self.entry.data[CONF_IPV4] is True:
-                errors[CONF_RESOLVER] = "invalid_resolver"
-            elif validate[CONF_IPV6] is False and self.entry.data[CONF_IPV6] is True:
-                errors[CONF_RESOLVER_IPV6] = "invalid_resolver"
-            else:
-                return self.async_create_entry(title=self.entry.title, data=user_input)
-
-        return self.async_show_form(
-            step_id="init",
-            data_schema=vol.Schema(
-                {
-                    vol.Optional(
-                        CONF_RESOLVER,
-                        default=self.entry.options.get(CONF_RESOLVER, DEFAULT_RESOLVER),
-                    ): cv.string,
-                    vol.Optional(
-                        CONF_RESOLVER_IPV6,
-                        default=self.entry.options.get(
-                            CONF_RESOLVER_IPV6, DEFAULT_RESOLVER_IPV6
-                        ),
-                    ): cv.string,
                 }
             ),
             errors=errors,
